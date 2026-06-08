92fd1073072820a810a6fb083b3455ec395cc8a3
Janio Isacura <janioisacura@gmail.com>
2026-06-08 17:58:40 -0300

    Add calculator feature with routing and view components

---
+++ src/app/calculator/views/calculator-view/calculator-view.ts
@@ -0,0 +1,8 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'calculator-view',
+  imports: [],
+  templateUrl: './calculator-view.html',
+})
+export default class CalculatorView {}
